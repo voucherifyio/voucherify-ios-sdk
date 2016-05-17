0fa82fe661a259c63506268bb38586c09fd7600d
marcin.polak <mpolak87@gmail.com>
2016-05-17 21:09:58 +0200

    init empty workspace

---
+++ ios-sdk/ios-sdk/ios-sdk.h
@@ -0,0 +1,19 @@
+//
+//  ios-sdk.h
+//  ios-sdk
+//
+//  Created by Marcin Polak on 17.05.2016.
+//  Copyright © 2016 rspective P. Rychlik sp. j. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ios-sdk.
+FOUNDATION_EXPORT double ios_sdkVersionNumber;
+
+//! Project version string for ios-sdk.
+FOUNDATION_EXPORT const unsigned char ios_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ios_sdk/PublicHeader.h>
+
+
