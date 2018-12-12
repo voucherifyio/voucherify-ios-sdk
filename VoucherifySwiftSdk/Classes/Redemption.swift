6b51926f1efa9c39b2acbf0fbe87454761d174e6
Marcin Polak <marcin.polak@rspective.pl>
2018-12-12 19:36:13 +0100

    Redemption cleans up
    Update methods comments + weak selfs

--- VoucherifySwiftSdk/Classes/Redemption.swift
+++ VoucherifySwiftSdk/Classes/Redemption.swift
@@ -10,20 +10,29 @@ public class Redemption {
 	}
 	
 	/**
-	   Increments redemption counter and updates history of the voucher
-	   - parameter code: Voucher's code which we want to redeem
-	   - parameter completion: response callback function
-	   */
+	Redeem increments redemption counter and updates history of the voucher
+
+	- Parameters:
+		- code: a voucher's which will be redeemed
+		- completion: a server response callback block
+ 	*/
 	public func redeem(code: String,
 					   redeemContext: VoucherRedemptionContext? = nil,
 					   completion: @escaping (_ response: Result<VoucherRedemptionResult>) -> Void) {
+		
 		var params = httpClient.getBaseQueryParams()
 		
 		params[HttpQueryParamName.code] = code as AnyObject?
 		
-		httpClient.request(requestUrl: VoucherifyRouter.redeemVoucher(params, redeemContext) as URLRequestConvertible) { (response) in
-			completion(self.httpClient.handleJsonResponse(response: response))
-		}
+		httpClient.request(
+			requestUrl: VoucherifyRouter.redeemVoucher(params, redeemContext) as URLRequestConvertible,
+			completion: {
+				[weak self] (response) in
+				guard let strongSelf = self else {
+					return
+				}
+				
+				completion(strongSelf.httpClient.handleJsonResponse(response: response))
+			})
 	}
-	
 }
