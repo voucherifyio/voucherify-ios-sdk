7692c8a86242162bdff42902bbfa6b41767569fb
Marcin Polak <marcin.polak@rspective.pl>
2019-01-27 07:14:01 +0100

    Add comments for Voucher list API

--- VoucherifySwiftSdk/Classes/Api/VoucherApi.swift
+++ VoucherifySwiftSdk/Classes/Api/VoucherApi.swift
@@ -3,12 +3,26 @@ import Foundation
 
 public protocol VoucherApi {
 	
+	/**
+	List allows to fetch all vouchers or filter them by passing customer parameter
+
+	- Parameters:
+		- customer: an optional id of the customer
+ 		- completion: a server response callback block
+	*/
 	func list(customer: String?,
 			  completion: @escaping (_ response: Result<VoucherList>) -> Void)
 }
 
 public extension VoucherApi {
 	
+	/**
+	List allows to fetch all vouchers or filter them by passing customer parameter
+
+	- Parameters:
+		- customer: an optional id of the customer
+		- completion: a server response callback block
+	*/
 	func list(customer: String? = nil,
 			  completion: @escaping (Result<VoucherList>) -> Void) {
 		
--- VoucherifySwiftSdk/Classes/Models/VoucherList.swift
+++ VoucherifySwiftSdk/Classes/Models/VoucherList.swift
@@ -1,14 +1,21 @@
 import Foundation
 import ObjectMapper
 
+/**
+ * I hold a list of vouchers
+ */
 public struct VoucherList: Mappable {
 	
+	// Vouchers array for a given filtering query
 	public var vouchers: [Voucher] = []
 	
+	// Total number of vouchers for given filtering query
 	public var total: Int = 0
 	
+	// A string describing the object type returned
 	public var object: String?
 	
+	// A value for this property directs to the property name which points to the results array
 	public var dataRef: String?
 	
 	public init?(map: Map) {
